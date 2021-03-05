e461671e540747cb65a8464a243394fb7f5c53fc
Amadeu2018 <amadeualexandrealeg@hotmail.com>
2021-03-06 00:13:38 +0100

    commit inicial

---
+++ src/test/java/com/alexandre/amasoft/AmasoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandre.amasoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmasoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
